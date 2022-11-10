da96438f605b115de8a0db9097c14deeecf16411
marievolsen <mvol22@student.aau.dk>
2022-11-10 21:37:26 +0100

    intet nyt

--- P1_Project/Assets/Scripts/Buttons.cs
+++ P1_Project/Assets/Scripts/Buttons.cs
@@ -3,31 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Buttons : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+  public void LoadScene(string sceneName)
     {
       SceneManager.LoadScene(sceneName);
-
-      
-    } 
-
-void update()
-{
-IEnumerator DelayedSceneChange()
-    {
-      yield return new WaitForSecondsRealtime(10f);
-      
-    
-      {
-        StartCoroutine(DelayedSceneChange());
-      }
-      
     }
+} 
 
-
-}
-}
 
 
 
