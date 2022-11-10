b0c1969f92782fe499fccde12efcb40ae829ba52
marievolsen <mvol22@student.aau.dk>
2022-11-10 13:06:06 +0100

    lille ændring

--- P1_Project/Assets/Scripts/Buttons.cs
+++ P1_Project/Assets/Scripts/Buttons.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class Buttons : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+      SceneManager.LoadScene(sceneName);
     }
 }
+
+
+IEnumerator DelayedSceneChange(float delay, string sceneName)
+{
+    yield return new WaitForSecondsRealtime(delay);
+    LoadScene(sceneName);
+    
+    StartCoroutine(DelayedSceneChange(20f, sceneName));
+}
+
 
 
 
