251110dae19a9fa81c2dca652d7c9510e2909044
Slente468 <GL51HW@student.aau.dk>
2022-12-07 19:34:43 +0100

    Age Buttons added
    as well as being able to get to specific scenes

---
+++ P1_Project/Assets/Scripts/AgeSelect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AgeSelect : MonoBehaviour
+{
+    // Q1 S1 is text
+    public string scene;
+  
+    // method that opens a variable string: scene by ToString
+       public void OpenScene()
+        {
+            SceneManager.LoadScene(scene.ToString());
+        }
+  
+}
