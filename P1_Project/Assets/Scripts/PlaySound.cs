9e223bdbc8a339624a466aa390cef5f79e92f84b
Slente468 <GL51HW@student.aau.dk>
2022-12-08 15:38:21 +0100

    Delay between Audio files

--- P1_Project/Assets/Scripts/PlaySound.cs
+++ P1_Project/Assets/Scripts/PlaySound.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     private AudioSource _audioSource;
+    public float timeBetweenPlays = 10f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Kalder sounddPlayer hvert timeBetweenPlays sekund (base af 10)
+        InvokeRepeating("sounddPlayer", timeBetweenPlays, timeBetweenPlays);
+
+        // får fat på audiosource, spiller den hvis den kan finde den.
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void sounddPlayer()
     {
-
+        // hvis den ikke har en audiosource printer denne i debug log
         if (_audioSource == null)
         {
             Debug.LogError("There is no sound connected to the Question in the set");
         }
         else
         {
-            _audioSource = GetComponent<AudioSource>();
             _audioSource.Play();
         }
     }
